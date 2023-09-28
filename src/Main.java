06e2a5d44b05b7ff87f5ceacd2961c398655efa7
amakhanova <annanen@inbox.ru>
2023-09-28 23:29:44 +0300

    HW1 is done, please review

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         System.out.println("Here are some more changes");
         System.out.println("Please also review, thanks!");
+        System.out.println("Aaand some more changes again...");
     }
 }
